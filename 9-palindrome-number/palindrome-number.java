d5cf47d3c83a08c4357bff5b15a779adcd861c19
Antara Dhar <161041911+antara-dhar@users.noreply.github.com>
2026-06-04 19:05:09 +0530

    Time: 5 ms (78.57%) | Memory: 45.9 MB (71.33%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int rev=0;
+        while(x>0){
+            int d=x%10;
+            rev=rev*10+d;
+            x=x/10;
+        }
+        if(rev==temp)
+        return true;
+        else
+        return false;
+    }
+}
