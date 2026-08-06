609f854a7c4659be4f2239c747021e440ea1a17e
Antara Dhar <161041911+antara-dhar@users.noreply.github.com>
2026-08-06 13:51:41 +0530

    Time: 19 ms (10.39%) | Memory: 46.4 MB (42.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,10 +3,21 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char a[]=s.toCharArray();
-        char b[]=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
+        HashMap<Character,Integer> map= new HashMap<>();
+        for(int i= 0;i<s.length();i++){
+            char a=s.charAt(i);
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            char b=t.charAt(i);
+            if(!map.containsKey(b)){
+                return false;
+            }
+            map.put(b,map.get(b)-1);
+            if(map.get(b)==0){
+                map.remove(b);
+            }
+        }
+        return true;
     }
 }
