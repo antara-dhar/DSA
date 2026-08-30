f8d525a5d7521fbb2f1ec85eca3d88edec8e347b
Antara Dhar <161041911+antara-dhar@users.noreply.github.com>
2026-08-30 21:45:23 +0530

    Time: 8 ms (6.85%) | Memory: 46.3 MB (5.82%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,21 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String st= String.valueOf(x);
-        String str=st;
-        char ch[]=st.toCharArray();
-        int j=st.length()-1;
-        for(int i=0;i<st.length()/2;i++){
-            char temp=ch[i];
-            ch[i]=ch[j];
-            ch[j]=temp;
-            j--;
-        }
-        String cmp=new String(ch);
-        if(cmp.equals(st)){
-            return true;
-        }
-        else{
-            return false;
-        }
+        String st=String.valueOf(x);
+        String rev = new StringBuilder(st).reverse().toString();
+        return rev.equals(st);
     }
 }
