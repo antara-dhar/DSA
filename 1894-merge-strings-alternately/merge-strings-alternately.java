6f61449b36b7b58153f39f6deb0ca8de887bbce3
Antara Dhar <161041911+antara-dhar@users.noreply.github.com>
2026-09-18 18:12:05 +0530

    Time: 1 ms (95.71%) | Memory: 43.3 MB (34.81%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,23 +1,19 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder temp = new StringBuilder();
-        int n1=word1.length();
-        int n2=word2.length();
-        int i=0;
-        int j=0;
-        while(i<n1 && j<n2){
-            temp.append(word1.charAt(i++));
-            temp.append(word2.charAt(j++));
+        int i=0; int j=0;
+        StringBuilder sb = new StringBuilder();
+        while(i<word1.length() && j<word2.length()){
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(j));
+            i++;
+            j++;
         }
-        while(i<n1){
-            temp.append(word1.charAt(i++));
-
+        while(i<word1.length()){
+            sb.append(word1.charAt(i++));
         }
-        while(j<n2)
-        {
-            temp.append(word2.charAt(j++));
-           
+        while(j<word2.length()){
+            sb.append(word2.charAt(j++));
         }
-        return temp.toString();
+        return sb.toString();
     }
 }
