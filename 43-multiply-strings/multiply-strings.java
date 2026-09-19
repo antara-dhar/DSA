37e35e38012924e41d91e77c8c0459e14477e58d
Antara Dhar <161041911+antara-dhar@users.noreply.github.com>
2026-09-20 00:27:54 +0530

    Time: 3 ms (77.38%) | Memory: 43.5 MB (79.39%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,43 +1,31 @@
 class Solution {
     public String multiply(String num1, String num2) {
-
-        if (num1.equals("0") || num2.equals("0")) {
+        int m=num1.length();
+        int n=num2.length();
+        int result[]= new int[m+n];
+        if(num1.equals("0") || num2.equals("0")){
             return "0";
         }
-
-        int[] result = new int[num1.length() + num2.length()];
-
-        // Multiply every digit
-        for (int i = num1.length() - 1; i >= 0; i--) {
-
-            for (int j = num2.length() - 1; j >= 0; j--) {
-
-                int n1 = num1.charAt(i) - '0';
-                int n2 = num2.charAt(j) - '0';
-
-                int product = n1 * n2;
-
-                int p1 = i + j;
-                int p2 = i + j + 1;
-
-                int sum = product + result[p2];
-
-                result[p2] = sum % 10;
-                result[p1] += sum / 10;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int n1=num1.charAt(i)-'0';
+                int n2=num2.charAt(j)-'0';
+                int product=n1*n2;
+                int p1=i+j;
+                int p2=i+j+1;
+                int sum=product+result[p2];
+             result[p2]=sum%10;
+             result[p1]+=sum/10;
             }
         }
-
-        // Convert array to String
-        StringBuilder ans = new StringBuilder();
-
-        for (int digit : result) {
-            if (ans.length() == 0 && digit == 0) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<result.length;i++){
+            if(sb.length()==0 && result[i]==0){
                 continue;
             }
+            sb.append(result[i]);
 
-            ans.append(digit);
         }
-
-        return ans.toString();
+        return sb.toString();
     }
 }
